e93bdf7ddf10646e310dca9d08c62e1fdbce4f60
Leonardo Medina <leo_medina14@hotmail.com>
2021-09-30 22:14:42 -0500

    Fundamentos de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './produc.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,91 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'leo-store-two';
+
+  widthImg = 10;
+  name = 'Nicolas';
+  age = 18;
+  img = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisabled = true;
+  register = {
+    name: '',
+    email: '',
+    password: '',
+  }
+  person = {
+    name: 'Nicolas',
+    age: 18,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
+  }
+  names: string[] = ['Nico', 'Juli', 'Santi'];
+  newName = '';
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red'
+  };
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/Ojos.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/ojo_senjut.png'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/ojo_senjut_y_kyubi.png'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/ojo_naruto.png'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/ojo_kuybi.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/ojo_iris_senjut_kyyubi.jpg'
+    }
+  ]
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
+
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number) {
+    this.names.splice(index, 1);
+  }
+
+  onRegister() {
+    console.log(this.register);
+  }
 }
