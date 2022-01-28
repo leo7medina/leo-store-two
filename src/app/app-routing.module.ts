9175fc1d06ed70d78456337c664d9b7d8eabbea6
Leonardo Medina <leo_medina14@hotmail.com>
2022-01-28 00:02:18 -0500

    Init course Angular Router - creation routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {CategoryComponent} from "./pages/category/category.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {MycartComponent} from "./pages/mycart/mycart.component";
+import {NotFoundComponent} from "./pages/not-found/not-found.component";
+import {ProfileComponent} from "./pages/profile/profile.component";
+import {RegisterComponent} from "./pages/register/register.component";
+import {RecoveryComponent} from "./pages/recovery/recovery.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'category', component: CategoryComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'my-cart', component: MycartComponent},
+  { path: 'not-found', component: NotFoundComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'recovery', component: RecoveryComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
