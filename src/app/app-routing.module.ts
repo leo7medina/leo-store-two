a68369f05351fda846eea78113415779aa95a018
Leonardo Medina <leo_medina14@hotmail.com>
2022-01-30 02:46:39 -0500

    add website module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./website/pages/home/home.component";
-import {CategoryComponent} from "./website/pages/category/category.component";
-import {LoginComponent} from "./website/pages/login/login.component";
-import {MycartComponent} from "./website/pages/mycart/mycart.component";
 import {NotFoundComponent} from "./website/pages/not-found/not-found.component";
-import {ProfileComponent} from "./website/pages/profile/profile.component";
-import {RegisterComponent} from "./website/pages/register/register.component";
-import {RecoveryComponent} from "./website/pages/recovery/recovery.component";
-import {ProductDetailComponent} from "./website/pages/product-detail/product-detail.component";
-import {LayoutComponent} from "./website/components/layout/layout.component";
 
 const routes: Routes = [
-  { path: '',
-    component: LayoutComponent,
-    children: [
-      { path: '', redirectTo: '/home', pathMatch: 'full'},
-      { path: 'home', component: HomeComponent},
-      { path: 'category/:id', component: CategoryComponent},
-      { path: 'product/:id', component: ProductDetailComponent},
-      { path: 'login', component: LoginComponent},
-      { path: 'my-cart', component: MycartComponent},
-      { path: 'profile', component: ProfileComponent},
-      { path: 'register', component: RegisterComponent},
-      { path: 'recovery', component: RecoveryComponent},
-    ]
+  {
+    path: '',
+    loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule)
   },
   {
     path: 'cms',
